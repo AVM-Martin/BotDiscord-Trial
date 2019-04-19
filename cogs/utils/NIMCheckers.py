649df3ce170fa18b4c0558d772b41e8d5473053b
AVM.Martin <avmm.martin+github@gmail.com>
2019-04-19 21:43:36 +0700

    Refactor StudentId based on PEP8

--- cogs/utils/NIMCheckers.py
+++ cogs/utils/NIMCheckers.py
@@ -1,26 +1,32 @@
-class NIM:
-    def __init__(self, NIM):
-        self._NIM = NIM;
+"""NIM Checkers"""
 
-    def isValid(self):
-        nums = [int(c) for c in self._NIM if("0" <= c <= "9")];
+# class StudentId:
+class NIM:
+    def __init__(self, student_id):
+        self._student_id = student_id;
+
+    def is_valid(self):
+        nums = [int(num) for num in self._student_id if '0' <= num <= '9'];
+
         # check length
         if(len(nums) != 10):
             return False;
+
         # check last digit
         if(nums[9] > 6):
             return False;
+
         # check sum digit
-        for i in range(9):
-            if(i%2 == 0):
-                nums[i] = (4*nums[i]) % 7;
-            if(i%2 == 1):
-                nums[i] = (14-nums[i]) % 7;
-        res = 0;
+        for idx in range(9):
+            if(idx%2 == 0):
+                nums[idx] = (4*nums[idx]) % 7;
+            if(idx%2 == 1):
+                nums[idx] = (14-nums[idx]) % 7;
+        sum_ = 0;
         for num in nums:
-            res = (res + num) % 7;
-        return res == 4;
+            sum_ = (sum_ + num) % 7;
+        return sum_ == 4;
 
     def __str__(self):
-        nums = [c for c in self._NIM if("0" <= c <= "9")];
+        nums = [num for num in self._student_id if '0' <= num <= '9'];
         return "".join(nums);
